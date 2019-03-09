37d905cc0c7856c3e5edd23880475cd97c6cd23a
Pemba <tamangtshering5@gmail.com>
2019-03-09 18:06:44 +0545

    this is my third commit

--- saratimes/resources/views/backend/include/footer.blade.php
+++ saratimes/resources/views/backend/include/footer.blade.php
@@ -99,35 +99,3 @@
 
 </script>
 
-{{--<script src="{{URL::to('backend/js/bootstrap.min.js')}}"></script>
-
-<!-- gauge js -->
-<script type="text/javascript" src="{{URL::to('backend/js/gauge/gauge.min.js')}}"></script>
-<script type="text/javascript" src="{{URL::to('backend/js/gauge/gauge_demo.js')}}"></script>
-<!-- bootstrap progress js -->
-<script src="{{URL::to('backend/js/progressbar/bootstrap-progressbar.min.js')}}"></script>
-<script src="{{URL::to('backend/js/nicescroll/jquery.nicescroll.min.js')}}"></script>
-<!-- icheck -->
-<script src="{{URL::to('backend/js/icheck/icheck.min.js')}}"></script>
-<!-- daterangepicker -->
-<script type="text/javascript" src="{{URL::to('backend/js/moment/moment.min.js')}}"></script>
-<script type="text/javascript" src="{{URL::to('backend/js/datepicker/daterangepicker.js')}}"></script>
-<!-- chart js -->
-<script src="{{URL::to('backend/js/chartjs/chart.min.js')}}"></script>
-
-<script src="{{URL::to('backend/js/custom.js')}}"></script>
-
-<!-- flot js -->
-<!--[if lte IE 8]><script type="text/javascript" src="{{URL::to('backend/js/excanvas.min.js')}}"></script><![endif]-->
-<script type="text/javascript" src="{{URL::to('backend/js/flot/jquery.flot.js')}}"></script>
-<script type="text/javascript" src="{{URL::to('backend/js/flot/jquery.flot.pie.js')}}"></script>
-<script type="text/javascript" src="{{URL::to('backend/js/flot/jquery.flot.orderBars.js')}}"></script>
-<script type="text/javascript" src="{{URL::to('backend/js/flot/jquery.flot.time.min.js')}}"></script>
-<script type="text/javascript" src="{{URL::to('backend/js/flot/date.js')}}"></script>
-<script type="text/javascript" src="{{URL::to('backend/js/flot/jquery.flot.spline.js')}}"></script>
-<script type="text/javascript" src="{{URL::to('backend/js/flot/jquery.flot.stack.js')}}"></script>
-<script type="text/javascript" src="{{URL::to('backend/js/flot/curvedLines.js')}}"></script>
-<script type="text/javascript" src="{{URL::to('backend/js/flot/jquery.flot.resize.js')}}"></script>--}}
-
-</body>
-</html>
